4bef79fea0f7e51140e06d6d65dbc0fa0499e3ea
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2022-10-10 22:34:02 +0400

    users and labeling jobs automation tutorials (#2)
    * user-automation guide (WIP)
    
    * user-automation guide (WIP)
    
    * labeling-jobs-automation WIP
    
    * update labeling-jobs tutorial
    
    * update labeling-jobs tutorial
    
    * add images, update main.py
    
    * update labeling-jobs tutorial
    
    * update labeling-jobs tutorial
    
    * fix typos
    
    * separate output
    
    * admin permission notes
    
    * Users automation (#1)
    
    * add images
    
    * update readme
    
    * update images
    
    * fix typos
    
    * separate output
    
    * update users-automation
    
    * update users-automation
    
    * admin permission notes
    
    * admin permission notes

---
+++ examples/labeling-jobs-automation/main.py
@@ -0,0 +1,97 @@
+import os
+from dotenv import load_dotenv
+import supervisely as sly
+
+from prepare_project import prepare_project
+
+# for debug
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+load_dotenv("local.env")
+api = sly.Api()
+
+TEAM_ID = int(os.environ["CONTEXT_TEAMID"])
+PROJECT_ID = int(os.environ["CONTEXT_PROJECTID"])
+USER_ID = int(os.environ["CONTEXT_USERID"])
+USER_LOGIN = os.environ["CONTEXT_USERLOGIN"]
+
+# populate project meta with classes and tag metas
+prepare_project(api=api, id=PROJECT_ID)
+
+# Step 1. Create and add annotators to the team, before creating Labeling Job
+
+# create accounts for annotators with restrictions (admin permission required)
+labeler_1 = api.user.get_info_by_login(login='labeler_1')
+if labeler_1 is None:
+    labeler_1 = api.user.create(login='labeler_1', password='11111abc', is_restricted=True)
+
+labeler_2 = api.user.get_info_by_login(login='labeler_2')
+if labeler_2 is None:
+    labeler_2 = api.user.create(login='labeler_2', password='22222abc', is_restricted=True)
+
+# labelers will be able to login only after being added to at least one team (admin permission required)
+if api.user.get_team_role(labeler_1.id, TEAM_ID) is None:
+    api.user.add_to_team(labeler_1.id, TEAM_ID, api.role.DefaultRole.ANNOTATOR)
+if api.user.get_team_role(labeler_2.id, TEAM_ID) is None:
+    api.user.add_to_team(labeler_2.id, TEAM_ID, api.role.DefaultRole.ANNOTATOR)
+
+# Step 2. Define project and datasets for labeling job
+project_meta_json = api.project.get_meta(PROJECT_ID)
+project_meta = sly.ProjectMeta.from_json(project_meta_json)
+print(project_meta)
+
+datasets = api.dataset.get_list(PROJECT_ID)
+print(datasets)
+
+# Labeler 1 will label lemons on the first dataset
+created_jobs = api.labeling_job.create(name='labeler1_lemons_task',
+                                       dataset_id=datasets[0].id,
+                                       user_ids=[labeler_1.id],
+                                       readme='annotation manual for fruits in markdown format here (optional)',
+                                       description='short description is here (optional)',
+                                       classes_to_label=["lemon"])
+print(created_jobs)
+
+# Stop Labeling Job, job will be unavailable for labeler
+api.labeling_job.stop(created_jobs[0].id)
+
+# Labeler 2 will label kiwis on the first dataset
+created_jobs = api.labeling_job.create(name='labeler2_kiwi_task_with_complex_settings',
+                                       dataset_id=datasets[0].id,
+                                       user_ids=[labeler_2.id],
+                                       readme='annotation manual for fruits in markdown format here (optional)',
+                                       description='short description is here (optional)',
+                                       classes_to_label=["kiwi"],
+                                       objects_limit_per_image=10,
+                                       tags_to_label=["size", "origin"],
+                                       tags_limit_per_image=20,
+                                       )
+print(created_jobs)
+
+# Get all labeling jobs in a team
+jobs = api.labeling_job.get_list(TEAM_ID)
+print(jobs)
+
+# Labeling Jobs Filtering (filters [created_by_id, assigned_to_id, project_id, dataset_id] can be used in various combinations)
+# Get all labeling jobs that were created by yourself
+user = api.user.get_info_by_login(USER_LOGIN) # getting UserInfo by login requires admin permission
+jobs = api.labeling_job.get_list(TEAM_ID, created_by_id=user.id)
+print(jobs)
+
+# Get all labeling jobs that were created by yourself and were assigned to labeler 2
+jobs = api.labeling_job.get_list(TEAM_ID, created_by_id=user.id, assigned_to_id=labeler_2.id)
+print(jobs)
+
+# Get all active labeling jobs in a team
+jobs = api.labeling_job.get_list(TEAM_ID)
+print(jobs)
+
+# Labeling Jobs Statuses
+job_id = jobs[-2].id
+api.labeling_job.get_status(job_id)
+# <Status.STOPPED: 'stopped'>
+job_id = jobs[-1].id
+api.labeling_job.get_status(job_id)
+# <Status.PENDING: 'pending'>
+
+# Archive Labeling Job
+api.labeling_job.archive(jobs[0].id)
---
+++ examples/users-automation/main.py
@@ -0,0 +1,88 @@
+import os
+from dotenv import load_dotenv
+import supervisely as sly
+
+# for debug
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+load_dotenv("local.env")
+api = sly.Api()
+
+USER_ID = int(os.environ["CONTEXT_USERID"])
+TEAM_ID = int(os.environ["CONTEXT_TEAMID"])
+USER_LOGIN = os.environ["CONTEXT_USERLOGIN"]
+
+# list available roles on Supervisely instance
+roles = api.role.get_list()
+print(f"Roles: {roles}")
+
+# get UserInfo about yourself
+my_info = api.user.get_my_info()
+print("my login:", my_info.login)
+print("my ID:", my_info.id)
+
+# get UserInfo by ID
+user_info = api.user.get_info_by_id(USER_ID)
+print(f"UserInfo by ID: {user_info}")
+
+# list all team users with corresponding roles
+team = api.team.get_info_by_id(TEAM_ID)
+members = api.user.get_team_members(team.id) 
+print(f"Team members: {members}")
+
+##############################################
+# All methods below require admin permission #
+##############################################
+
+# list all registered users
+users = api.user.get_list()
+for user in users:
+    print("Id: {:<5} Login: {:<25s} logins_count: {:<5}".format(user.id, user.login, user.logins))
+
+# get UserInfo by login
+user_info = api.user.get_info_by_login(USER_LOGIN)
+print(f"UserInfo by name: {user_info}")
+
+# list all user teams with corresponding roles
+def print_user_teams(login):
+    user = api.user.get_info_by_login(login)
+    user_teams = api.user.get_teams(user.id)
+    print("\nTeams of user {!r}:".format(login))
+    for team in user_teams:
+        print("[team_id={}] {:<25s} [role_id={}] {}".format(team.id, team.name, team.role_id, team.role))
+
+print_user_teams(USER_LOGIN)
+
+# create new user
+new_user = api.user.get_info_by_login('demo_user_451')
+if new_user is None:
+    new_user = api.user.create(login='demo_user_451', password='123abc', is_restricted=False)
+print(f"New UserInfo: {new_user}")
+
+# update UserInfo
+new_password = 'abc123'
+new_name = 'Bob'
+user_info = api.user.update(user_info.id, password=new_password, name=new_name)
+print(f"Updated UserInfo: {new_user}")
+
+
+# disable/enable user
+api.user.disable(user_info.id)
+api.user.enable(user_info.id)
+
+# invite user to team
+user = api.user.get_info_by_login('demo_user_451')
+team = api.team.get_info_by_id(TEAM_ID)
+if api.user.get_team_role(user.id, team.id) is None:
+    api.user.add_to_team(user.id, team.id, api.role.DefaultRole.ANNOTATOR) 
+    
+# change user role in team
+user = api.user.get_info_by_login('demo_user_451')
+team = api.team.get_info_by_id(TEAM_ID)
+api.user.change_team_role(user.id, team.id, api.role.DefaultRole.VIEWER)
+print_user_teams('demo_user_451')
+
+# remove user from team
+team = api.team.get_info_by_id(TEAM_ID)
+user = api.user.get_info_by_login('demo_user_451')
+api.user.remove_from_team(user.id, team.id)
+print_user_teams('demo_user_451')
