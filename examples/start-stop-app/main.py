54e2f2cd8090137e70d4f295a106cf49f5000c0c
Maxim Kolomeychenko <maxim@supervise.ly>
2022-10-10 06:14:15 +0300

    target in kwargs

--- examples/start-stop-app/main.py
+++ examples/start-stop-app/main.py
@@ -11,15 +11,16 @@ workspace_id = sly.env.workspace_id()
 agent_id = sly.env.agent_id()
 
 
-module_id = 83  # module ID of application in ecosystem
 module_id = api.app.get_ecosystem_module_id("supervisely-ecosystem/export-to-pascal-voc")
+# module_id = 83  # or copy module ID of application in ecosystem
 module_info = api.app.get_ecosystem_module_info(module_id)
 print("Start app: ", module_info.name)
 
 print("Help for developers:")
 module_info.arguments_help()
 
-params = module_info.get_arguments(target={"key": "slyProjectId", "value": 12489})
+# validation on server
+params = module_info.get_arguments(images_project=12489)
 
 # @TODO: return namedtuple
 # @TODO: fix queued
